9b258fd441ed165701d5707325614c6c16dbab70
zylgt <704892422@qq.com>
2020-06-04 14:38:29 +0800

    修改接口地址

--- config/dep.env.js
+++ config/dep.env.js
@@ -1,6 +1,6 @@
 module.exports = {
 	NODE_ENV: '"production"',
   ENV_CONFIG: '"dep"',
-   BASE_API: '"https://wechat.natapp4.cc/andonApi/"'
-  //BASE_API: '"https://wechat-dev.ihealthlabs.com.cn/andonApi/"'
+  //BASE_API: '"https://wechat.natapp4.cc/andonApi/"'
+  BASE_API: '"https://wechat-dev.ihealthlabs.com.cn/andonApi/"'
 }
--- config/dev.env.js
+++ config/dev.env.js
@@ -3,6 +3,6 @@ module.exports = {
   ENV_CONFIG: '"dev"',
   //BASE_API: '"http://localhost:3085/andonApi/"'
   // BASE_API: '"http://122.51.199.160:8080/admin"'
-  BASE_API: '"https://wechat.natapp4.cc/andonApi/"'
-  //BASE_API: '"https://wechat-dev.ihealthlabs.com.cn/andonApi/"'
+  //BASE_API: '"https://wechat.natapp4.cc/andonApi/"'
+  BASE_API: '"https://wechat-dev.ihealthlabs.com.cn/andonApi/"'
 }
