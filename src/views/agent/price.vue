970b3fb15762d935ef127823fae53ab269441ce4
zylgt <704892422@qq.com>
2020-06-04 17:39:32 +0800

    去掉编辑按钮

--- src/views/agent/price.vue
+++ src/views/agent/price.vue
@@ -23,7 +23,7 @@
 
       <el-table-column align="center" label="操作" width="300" class-name="small-padding fixed-width">
         <template slot-scope="scope">
-          <el-button type="info" size="mini" @click="handleUpdate(scope.row)">编辑</el-button>
+          <!-- <el-button type="info" size="mini" @click="handleUpdate(scope.row)">编辑</el-button> -->
           <el-button type="danger" size="mini" @click="handleDelete(scope.row)">删除</el-button>
         </template>
       </el-table-column>
