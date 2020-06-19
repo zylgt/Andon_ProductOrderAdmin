510d9f070c98088407f51926a218fa56e3010311
zylgt <704892422@qq.com>
2020-06-19 17:36:45 +0800

    文件管理去掉删除按钮

--- src/views/sys/role.vue
+++ src/views/sys/role.vue
@@ -28,7 +28,7 @@
       <el-table-column align="center" label="操作" class-name="small-padding fixed-width">
         <template slot-scope="scope">
           <el-button type="primary" size="mini" style="width:70px;" @click="handleUpdate(scope.row)">编辑文件</el-button>
-          <el-button type="danger" size="mini" @click="handleDelete(scope.row)">删除</el-button>
+          <!-- <el-button type="danger" size="mini" @click="handleDelete(scope.row)">删除文件</el-button> -->
         </template>
       </el-table-column>
     </el-table>
